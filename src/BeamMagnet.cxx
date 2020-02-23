90a039b7ee8b2f6738b283739c53b5d9eb3132ab
Jaroslav Adam <jaroslav.adam@cern.ch>
2020-02-22 20:24:59 -0500

    Beam magnet visibility on EIC nodes

--- src/BeamMagnet.cxx
+++ src/BeamMagnet.cxx
@@ -37,7 +37,7 @@ BeamMagnet::BeamMagnet(G4double zpos, G4LogicalVolume *top) {
   //vis->SetColor(1, 0, 1); // magenta
   vis->SetColor(0, 0, 1); // blue
   vis->SetLineWidth(2);
-  vis->SetForceSolid();
+  vis->SetForceSolid(true);
   //vis->SetForceAuxEdgeVisible(true);
   vol->SetVisAttributes(vis);
 
