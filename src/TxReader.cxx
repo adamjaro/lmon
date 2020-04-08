30e1b3999239eac35ad26eb549a7135cd85aab60
Jaroslav Adam <jaroslav.adam@cern.ch>
2020-04-08 17:07:42 -0400

    Simulations for Q^2 reconstruction

--- src/TxReader.cxx
+++ src/TxReader.cxx
@@ -101,10 +101,10 @@ void TxReader::GeneratePrimaries(G4Event *evt) {
   //G4cout << "TxReader::GeneratePrimaries: " << tmap[11]->GetPdg() << " " << tmap[22]->GetPdg() << G4endl;
 
   //generate the photon
-  tmap[22]->GenerateToVertex(vtx);
+  //tmap[22]->GenerateToVertex(vtx);
 
   //scattered electron
-  //tmap[11]->GenerateToVertex(vtx);
+  tmap[11]->GenerateToVertex(vtx);
 
   //put vertex to the event
   evt->AddPrimaryVertex(vtx);
